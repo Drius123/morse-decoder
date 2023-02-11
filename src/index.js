b9ebce1371fe66e259b4c853bca9ba5084c2c0d2
Андрей <adovgulev111@gmail.com>
2023-02-11 18:33:20 +0300

    good job

--- src/index.js
+++ src/index.js
@@ -38,9 +38,16 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	let word = "";
+	let symB = [];
+	let letter = [];
+	expr.split('').forEach((item, index, arr) => index % 2 == 0 ? symB.push(item + arr[index + 1]) : null);
+	symB = symB.map(item => {return item == '10' ? '.' : item == '11' ? '-' : item == '**' ? '*' : null});
+	symB.forEach((item, index) =>index % 5 == 0 && index != 0 ? letter.push(symB.slice(index - 5, index)) : index == symB.length - 1 ? letter.push(symB.slice(index - index % 5, index + 1)) : null);
+	letter = letter.map(item => {return item.filter(itm => itm != null)});
+	letter.forEach((item, index) => {item.join('') == '*****' ? word += ' ' : word += MORSE_TABLE[item.join('')]});
+	return word;
 }
-
 module.exports = {
     decode
 }
